327af720532ef31ff9b2622e3b38f91cfd754b78
ILgiza <ilgizagalievna66580@gmail.com>
2023-05-21 20:33:56 +0500

    сделала красивый вывод страницы

--- NewsPaper/article/views.py
+++ NewsPaper/article/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
+from datetime import datetime
 
 class NewsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'news'
-
+    queryset = Post.objects.order_by('-time')
 
 class NewsPage(DetailView):
     model = Post
