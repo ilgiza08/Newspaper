1d87e55ed95dd7cd5b4c89180123b4e21c1c943b
ILgiza <ilgizagalievna66580@gmail.com>
2023-07-10 15:44:09 +0500

    castom filters and tags

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware', 
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'NewsPaper.urls'
--- NewsPaper/article/templatetags/custom_filters.py
+++ NewsPaper/article/templatetags/custom_filters.py
@@ -1,14 +1,26 @@
 from django import template
- 
 register = template.Library()
+
+
+variants = ['скотина', 'сволочь', 'гадина', 'мурло',
+            'ублюдок', 'падла', 'сука', 'тварь', 'мразь', 'выродок',
+            'отродье', 'гнида', 'подонок', 'погань']
+
 
 @register.filter(name='Censor')
 def Censor(value):
     text = value.lower().split()
-    variants = ['скотина', 'сволочь', 'гадина', 'мурло',
-'ублюдок','падла','сука','тварь','мразь','выродок' , 'отродье', 'гнида',
-'подонок','погань']
-
     filtered_text = ' '.join([word for word in text if word not in variants])
     return filtered_text
 
+
+@register.filter(name='replace_letters')
+def replace_letters(text):
+    text = text.lower().split()
+    result = []
+    for word in text:
+        if word in variants:
+            result.append(word[0] + '*'*(len(word)-2) + word[-1])
+        else:
+            result.append(word)
+    return " ".join(result)
